5911b27bd33f40f27b9195b9902ee906c3a13862
John Doe <aditisingh07071994@gmail.com>
2025-12-24 23:26:11 -0800

    rem vitals

--- src/index.js
+++ src/index.js
@@ -9,18 +9,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// Report web vitals (optional)
-const reportWebVitals = (onPerfEntry) => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
-  }
-};
-
-reportWebVitals(console.log);
